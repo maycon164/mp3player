925210854da30a588193b41045e3423713e6b85c
maykeee <mayconfelipe164@gmail.com>
2021-12-29 07:30:48 -0300

    catching lyrics with nodejs

--- letras/app.js
+++ letras/app.js
@@ -10,31 +10,36 @@ app.listen(8080, () => {
     console.log("SERVIDOR RODANDO NA PORTA 8080");
 });
 
+app.get("", (req, res) => {
+    res.status(404).json({
+        message: "nibiruuu!!!!!"
+    })
+})
+
 app.get("/letra", (req, res) => {
 
-    let art = req.body.artista;
-    let musica = req.body.musica;
+    let art = (req.body.art ? req.body.art : req.query.art);
+    let music = (req.body.music ? req.body.music : req.query.music);
 
-    console.log(art, musica);
-
-    if (!art || !musica) {
+    if (!art || !music) {
         return res.status(500).json(
-            { resposta: "Preencha todos os campos" }
+            { ans: "Preencha todos os campos" }
         )
     }
 
-    let url = `https://api.vagalume.com.br/search.php?art=${art}&mus=${musica}`;
+    let url = `https://api.vagalume.com.br/search.php?art=${art}&mus=${music}`;
 
-    let letra;
-    // preciso pegar a letra na api do vagalumes
+    // i need to catch the lyrics from api vagalumes
     request(url, (error, response, body) => {
 
         if (!error && response.statusCode == 200) {
             let obj = JSON.parse(body);
 
             if (obj.mus[0]) {
-                letra = obj.mus[0].text;
-                return res.status(200).json({ letra });
+                lyrics = obj.mus[0].text;
+                paragraphs = lyrics.replace(/\n/g, '<br />')
+
+                return res.status(200).send(`<p>${paragraphs}</p>`);
             }
 
             return res.status(404).json("Não Encontrei essa musica")
